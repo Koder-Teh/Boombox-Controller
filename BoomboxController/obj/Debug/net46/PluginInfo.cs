6f58139437971f6c44fea6e0dc6ec3f833588e75
Koder-Tech <uihukgui455@mail.ru>
2024-01-05 16:13:50 +0700

    Uploading code

---
+++ BoomboxController/obj/Debug/net46/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace BoomboxController
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "BoomboxController";
+        public const string PLUGIN_NAME = "BoomboxController";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
